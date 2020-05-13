dc40a8b8e295768ec28b59a1ca74906afe72b3ba
Kapil Pramod Deshmukh <44638458+SnowyKay@users.noreply.github.com>
2020-05-13 06:25:25 +0530

    second

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 const globalUrls = {
     "mongoUri":"mongodb+srv://shrek:vrockypokey@cluster0-sfpu3.gcp.mongodb.net/test?retryWrites=true&w=majority",
-    "baseUrl" : "https://tiny-tree.herokuapp.com" 
+    "baseUrl" : "https://tdtr.herokuapp.com" 
 }
 
 module.exports = globalUrls;
