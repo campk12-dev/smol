3f49668bc52a7b7a519e3a013c41e5c6713bd1e4
Gabrielle <campk12-dev.gmail.com>
2020-11-27 19:50:23 +0530

    New commit with all modules and updated URL

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 const globalUrls = {
     "mongoUri":"mongodb+srv://shrek:vrockypokey@cluster0-sfpu3.gcp.mongodb.net/test?retryWrites=true&w=majority",
-    "baseUrl" : "https://tdtr.herokuapp.com" 
+    "baseUrl" : "https://silly-bell-a48422.netlify.app" 
 }
 
 module.exports = globalUrls;
